727fe05d423a27da83bb7726d5b24d8e30ef2917
caelondev <227796969+caelondev@users.noreply.github.com>
2026-07-14 16:44:56 +0800

    01: comments

--- complete/00-hello-world.zig
+++ complete/00-hello-world.zig
@@ -1,10 +1,5 @@
-//! Top-level doc
-//! documents the entire module
-
 const std = @import("std");
 
-/// Doc comment
 pub fn main() void {
-    // Comment
     std.debug.print("Hello, {s}!\n", .{"World"});
 }
